a0b2d46136223517e066d1081979b9b8e0b14ffe
omar <omar1807@stud.kea.dk>
2022-02-01 16:51:37 +0100

    one of three exercices done

--- Intro_Exercices/Strings.js
+++ Intro_Exercices/Strings.js
@@ -5,6 +5,11 @@ const numberOne = "1.10";
 const numberTwo = "2.30";
 
 // add those two numbers and show the result
+
+// I have used the parseFloat() method
+const sum = parseFloat(numberOne) + parseFloat(numberTwo);
+console.log("Sum of the two numbers: ", sum);
+
 // you cannot touch line 1 neither line 2
 
 
@@ -17,6 +22,9 @@ const numberTwo = "2.30";
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+// Used toFixed() function for two decimal conversion after parsing to float
+const sumInTwoDecimal = (parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)).toFixed(2);
+console.log("Sum in two decimal number: ", sumInTwoDecimal);
 
 // --------------------------------------
 // Exercise 3 - Decimals and average
@@ -26,9 +34,8 @@ const two = 45;
 const three = 98;
 
 // Show in the console the avg. with 5 decimals
-
-
-
+const average = ((one + two + three) / 3).toFixed(5);
+console.log("Average of three numbers: ", average);
 
 
 // --------------------------------------
@@ -37,17 +44,19 @@ const three = 98;
 const letters = "abc";
 // Get me the character "c"
 
-
+console.log("character c, in variable letters, is index 2 => ", letters[2]);
 
 
 // --------------------------------------
 // Exercise 5 - Replace
 
-const fact = "You are learning javascript!";
+let fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
+fact = fact.toLowerCase().split(' ');
+let factThree = fact[3][0].toUpperCase() + (fact[3].slice(1,))
 
-
+console.log("capitalized j: ", factThree);
 // --------------------------------------
 
 
